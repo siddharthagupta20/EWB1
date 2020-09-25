7c02cbb9f2d7950b6c21189bc7732fbc0bfc19b3
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-09-25 22:37:21 +0530

    modified master 'used different method for Random'

--- src/com/cg_ewb1/EmpWageBuilderOOPs.java
+++ src/com/cg_ewb1/EmpWageBuilderOOPs.java
@@ -1,10 +1,12 @@
 package com.cg_ewb1;
-
+import java.util.Random;
 public class EmpWageBuilderOOPs {
 	static int IS_FULL_TIME = 1;
 
 	public static void main(String[] args) {
-		double empCheck = Math.floor(Math.random() * 10 % 2);
+		Random rand=new Random();
+		
+		int  empCheck =rand.nextInt(2);
 		if (IS_FULL_TIME == empCheck) {
 			System.out.println("The employee is present.");
 		} else
