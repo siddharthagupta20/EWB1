b68e7d8d83dd9a4ac21bcaad2bfa13ebdb27ccfd
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-09-25 23:54:23 +0530

    modified emp_wage_class_methods 'added cond for multiple companies'

--- src/com/cg_ewb1/EmpWageBuilderOOPs.java
+++ src/com/cg_ewb1/EmpWageBuilderOOPs.java
@@ -3,17 +3,14 @@ package com.cg_ewb1;
 public class EmpWageBuilderOOPs {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 2;
-	public static final int MAX_HRS_IN_MONTH = 10;
 
-	public static int calEmpWages() {
+	public static int calEmpWages(String compName, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
 
 		int empHrs = 0;
 		int totalWorkingDays = 0;
 		int totalEmpHrs = 0;
 
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
@@ -30,15 +27,17 @@ public class EmpWageBuilderOOPs {
 			}
 			totalEmpHrs += empHrs;
 
-			System.out.println("Day#: " + totalWorkingDays + " Emo Hr: " + empHrs);
+			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs);
 
 		}
-		int totalEmpWage = totalEmpHrs + EMP_RATE_PER_HOUR;
+		int totalEmpWage = totalEmpHrs + empRatePerHour;
+		System.out.println("Total Emp Wages for Company: " + compName + " is: " + totalEmpWage);
 		return totalEmpWage;
 
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Total Emp Wages: " + calEmpWages());
+		calEmpWages("Capgemini", 25, 6, 10);
+		calEmpWages("Wipro", 17, 7, 5);
 	}
 }
